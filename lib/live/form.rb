00049f3a2fd8af4bdc5402e25199ab78d59345f3
pharmac1st <pierre.montelle@gmail.com>
2021-07-05 07:17:15 +1200

    added Form class

---
+++ lib/live/form.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require_relative 'view'
+require 'trenni/builder'
+require 'json'
+
+module Live 
+    #Represents a Form within the view layer
+    #Differs from Live::View because the server sends both the 
+    #details and a serialized form from the view layer.
+    class Form < Live::View 
+        
+        #returns [String] javascript code to execute when the form is submitted.
+        def handleForm(details=false)
+            if details 
+                return "live.handleForm(this.id, event, #{details})"
+            else 
+                return "live.handleForm(this.id, event)"
+            end 
+        end 
+
+        #Render the element
+        #returns [Object] Renders the given builder inside a form tag.
+        def to_html()
+
+            Trenni::Builder.fragment do |builder|
+                builder.tag :form, method: "post", id: @id, class: 'live', onsubmit: handleForm, data: @data do
+                    render(builder)
+                end
+            end
+        end 
+
+        #Handles an incoming event.
+        # @parameter load [String] the parsed message from the view layer, 
+        # which includes the details and the serialized form data.
+        def handle(event, message)
+            @recieved_form = details[1].to_h
+        end         
+
+    end 
+end 
