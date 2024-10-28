dced3586fc3275f021628b0f2a3c2ab9e3de208c
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-10-28 13:53:11 +1300

    Fix coverage + add external test.

---
+++ config/sus.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Released under the MIT License.
+# Copyright, 2024, by Samuel Williams.
+
+$LOAD_PATH << ::File.expand_path("../ext", __dir__)
+
+require "covered/sus"
+include Covered::Sus
